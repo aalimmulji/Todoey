c049d1bb8b3eda1279fc762873caca4c48227278
Aalim Mulji <aalimmulji@aalims-mbp.home>
2018-07-30 20:20:38 -0400

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Aalim Mulji on 7/30/18.
+//  Copyright © 2018 Aalim Mulji. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done: Bool = false
+}
